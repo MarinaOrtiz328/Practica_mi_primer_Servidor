f43c5dc3b68584b9673f60a0134054000ca35e5c
Marina Ortiz <a329577@uach.mx>
2021-05-03 15:30:22 -0600

    se completa este punto

--- asincrono_segun_ext.js
+++ asincrono_segun_ext.js
@@ -3,16 +3,36 @@ const fs= require('fs');
 
 //http => (request, response)
 //const data= fs.readFileSync('./WWW/file.txt');
-
+// HTML, CSS, JS, IMG, sonido, video
 http.createServer((request, response)=>{
   console.log(request.url);
-  const file = request.url=='/'? './myfile.txt': `.${request.url}`;
+  const file = request.url=='/'? './WWW/index.html': `./WWW${request.url}`;
   fs.readFile(file, (err,data)=>{
     if(err){
       response.writeHead(404, {"Content.Type": "text/plain"});
       response.write("not found");
       response.end();
     }else{
+      //"hola.como estas"=> split(".").pop()
+      const extension = file.split('.').pop();
+      switch (extension){
+        case 'txt':
+          response.writeHead(200, {"Content.Type": "text/plain"});
+          break;
+        case 'html':
+          response.writeHead(200, {"Content.Type": "text/html"});
+          break;
+        case 'jpeg':
+          response.writeHead(200, {"Content.Type": "image/jpeg"});
+          break;
+        case 'css':
+          response.writeHead(200, {"Content.Type": "text/css"});
+          break;
+        case 'js':
+          response.writeHead(200, {"Content.Type": "text/javascript"});
+          break;
+        default:
+      }
       response.writeHead(200, {"Content.Type": "text/plain"});
       response.write(data);
       response.end();
